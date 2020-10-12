b2e903f5fd42b05ba3e42e1685676ed4aecc330f
Guillermo Pérez <guillermojperezp@gmail.com>
2020-10-12 18:08:12 -0400

    docs commit

---
+++ docs/precache-manifest.486fa764c67ef047f7b8c4cfb872f757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5985f8518de662cf1d482c760b0bf8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc846ba776486d4ec647",
+    "url": "/static/css/main.7ad042b6.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc846ba776486d4ec647",
+    "url": "/static/js/main.97bc4a59.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
